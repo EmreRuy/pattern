4342dae517a82b76184f242d7d38235ebe443a10
EmreRuy <uyar.em.eu@gmail.com>
2025-10-23 21:02:14 +0300

    Update: Card sizes

--- app/src/main/java/com/example/pattern/ui/screens/homeScreen/components/HabitBuildCard.kt
+++ app/src/main/java/com/example/pattern/ui/screens/homeScreen/components/HabitBuildCard.kt
@@ -43,7 +43,7 @@ fun HabitBuildCard(
     ) {
         Row(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(22.dp)
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
